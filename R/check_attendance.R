a9a4dda5a0133f4583b44b8e8119154f7d94423d
your_username <your_email@example.com>
2014-12-12 13:57:14 -0500

    completed check_attendance() function

--- R/check_attendance.R
+++ R/check_attendance.R
@@ -17,40 +17,12 @@ check_attendance <- function(log = 'upload_log.txt', att_file = 'attendance_uplo
   log <- clean_log(log)
   att <- clean_attendance(att)
   
-  
-  log$Error_Row<-as.integer(as.vector(unlist(log[1])))
-  att$row<-c(1:nrow(att))
-  att_log<-inner_join(att,log,by=c("row"="Error_Row"))
-  att_log$idName<-paste0(att_log$First.Name,att_log$Last.Name)
-  
-  error_names<-tolower(unique(att_log$idName))
-  error_names<-as.data.frame(error_names)
-  names(error_names)="error_names"
-  
-  
-  
-  
-  
-  
-  # Clean datasets
-  eto <- clean_eto(eto)
-  pro <- clean_pro(pro)
-  
-  # Compare names
-  names <- compare(pro = pro, eto = eto, var = 'id_name')
-  # Compare usi
-  usi <- compare(pro = pro, eto = eto, var = 'usi')
-  # Compare enrollment dates
-  start <- compare(pro = pro, eto = eto, var = 'start')
-  # Compare exit dates
-  exit <- compare(pro = pro, eto = eto, var = 'exit')
-  
-  # Combine dataframes
-  out <- dplyr::rbind_list(names, usi, start, exit)
+  # Identify problematic attendance records in original .csv file
+  out <- dplyr::left_join(log, att, by = 'error_row')
   
   # Save results in a .csv file
   if(!file.exists('output')) {dir.create('output')}
-  write.csv(out, file = 'output/pro_demographics.csv', na = "", row.names = FALSE)
+  write.csv(out, file = 'output/attendance_check.csv', na = "", row.names = FALSE)
   
   message("Check your working directory: Your results were saved in the output folder") 
 }
